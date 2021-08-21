b771495cbc31dcc11333812969641c748ae29cec
Ryan Andriant <ryanandriant@outlook.com>
2021-08-21 11:46:16 +0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ryanandriant@outlook.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,7 +24,7 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  /**
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
